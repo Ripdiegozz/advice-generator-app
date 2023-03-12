26234a6727cad0ec61671a09ba7e8a3bf4983c70
Diego Garcia <diegogarciag63@gmail.com>
2023-03-11 19:33:12 -0500

    responsive for desktop devices

--- src/container/AdviceBox.jsx
+++ src/container/AdviceBox.jsx
@@ -5,10 +5,10 @@ import React from 'react'
 function AdviceBox() {
   return (
     <div>
-      <div className="text-center w-[100%] mx-auto my-auto rounded-tr-lg rounded-tl-lg">
+      <div className="text-center w-[100%] mx-auto my-auto rounded-tr-lg rounded-tl-lg lg:max-w-[600px]">
           <AdviceCard/>
-          {/* <img src="/pattern-divider-desktop.svg" alt="divisor-desktop" className="w-full"/> */}
-          <img src="/pattern-divider-mobile.svg" alt="divisor-mobile" className="w-full bg-[rgba(49,58,73,255)] pb-20 rounded-br-lg rounded-bl-lg"/>
+          <img src="/pattern-divider-desktop.svg" alt="divisor-desktop" className="w-full bg-[rgba(49,58,73,255)] pb-20 rounded-br-lg rounded-bl-lg min-[320px]:max-md:hidden lg:relative"/>
+          <img src="/pattern-divider-mobile.svg" alt="divisor-mobile" className="w-full bg-[rgba(49,58,73,255)] pb-20 rounded-br-lg rounded-bl-lg md:hidden"/>
       </div>
       <DiceButton/>
     </div>
