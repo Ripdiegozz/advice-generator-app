90c960fed8a3ecc55e12e6df969e6f11b5849b61
Diego Garcia <diegogarciag63@gmail.com>
2023-03-12 23:13:42 -0500

    Text-color fix

--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AdviceCard() {
   
   return (
-      <div className='text-[hsl(123, 38%, 86%)] text-center py-6 px-10 bg-[rgba(49,58,73,255)] rounded-tr-lg rounded-tl-lg'>  
+      <div className='text-[#CEE9CF] text-center py-6 px-10 bg-[rgba(49,58,73,255)] rounded-tr-lg rounded-tl-lg'>  
         <p className='pt-12 pb-8 text-lg text-[#53ffab] tracking-[0.2rem] font-lato uppercase' id='advice-id'>ADVICE #20</p>
         <h2 className='text-3xl pb-12 font-serif font-bold' id='advice-quote'>"Life is better when you sing about bananas."</h2>
       </div>
